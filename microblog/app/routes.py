f57c18209401dfea69fbc37686c8a290b94fec47
arunkhattri <arunkhattri@outlook.com>
2018-03-26 19:07:42 +0530

    Added fake user

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Arun'}
+    posts = [
+        {
+            'author': {'username': "Aarush"},
+            'body': 'Beautiful day in Dhanbad'
+        },
+        {
+            'author': {'username': "Aaryaansh"},
+            'body': 'Where is my firefox cycle?'
+        },
+        {
+            'author': {'username': "Maira"},
+            'body': 'My only concern is Milk'
+        }
+    ]
+    return render_template('index.html', title='Home',
+                           user=user, posts=posts)
