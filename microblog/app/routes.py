5e04930a6659cac8dda5398fcd74e0a3f4e0ed4a
arunkhattri <arunkhattri@outlook.com>
2018-03-27 20:11:07 +0530

    Logging in and out users

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route('/')
@@ -24,11 +26,30 @@ def index():
     return render_template('index.html', title='Home',
                            user=user, posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if user logged in, redirect to index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    # otherwise serve login form
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # check if user exist, if yes password matches
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+
+        # register the user as logged in.
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
